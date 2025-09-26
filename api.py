a500908f675ce2053bf712219b4374eef28ced35
Cheick07 <cheicksorgho218@gmail.com>
2025-09-26 10:56:55 +0000

    api.py

--- api.py
+++ api.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
-from model.predictor import predict_match
+from pydantic import BaseModel
+from predictor import predict_match
 
 router = APIRouter()
 
+class MatchInput(BaseModel):
+    team_1_rating: float
+    team_2_rating: float
+
 @router.post("/predict")
-def get_prediction(data: dict):
-    return predict_match(data)
+def predict(input: MatchInput):
+    result = predict_match(input.team_1_rating, input.team_2_rating)
+    return {"prediction": result}
