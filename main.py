94c4244d540d64cca9df15fcc5c6eec7282da8f6
Cheick07 <cheicksorgho218@gmail.com>
2025-09-26 11:22:18 +0000

    main.py

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from fastapi import FastAPI
 from api import router
 
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Bienvenue sur l'API de Cheick"}
+
 app.include_router(router)
