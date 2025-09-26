b8fcd70c8767077a39e52f89786cbad1171c0b34
Cheick Hamed <cheicksorgho218@gmail.com>
2025-09-26 15:44:36 +0000

    Correction de la route /predict avec les 6 features

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# CORS pour autoriser Netlify
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Tu peux restreindre à ton domaine Netlify
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -17,7 +16,18 @@ app.add_middleware(
 class MatchInput(BaseModel):
     team_1_rating: float
     team_2_rating: float
+    team_1_rank: int
+    team_2_rank: int
+    team_1_odds: float
+    team_2_odds: float
 
 @app.post("/predict")
 def predict(input: MatchInput):
-    return predict_match(input.team_1_rating, input.team_2_rating)
+    return predict_match(
+        input.team_1_rating,
+        input.team_2_rating,
+        input.team_1_rank,
+        input.team_2_rank,
+        input.team_1_odds,
+        input.team_2_odds
+    )
